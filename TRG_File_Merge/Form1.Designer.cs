e59e1a1e18688b7facc0e2d1ab71f5b8456d3ab0
Brian-MacDonald-TRG <brian.macdonald@techresearchgroup.com>
2017-01-11 15:54:44 -0500

    Add project files.

---
+++ TRG_File_Merge/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace TRG_File_Merge
+{
+    partial class frmFileMerge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFileMerge));
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.btnMergeExport = new System.Windows.Forms.Button();
+            this.btnAddFolder = new System.Windows.Forms.Button();
+            this.gbxMergeOptions = new System.Windows.Forms.GroupBox();
+            this.txtCommentText = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.rbtCommentDelimiter = new System.Windows.Forms.RadioButton();
+            this.rbtSpaceDelimiter = new System.Windows.Forms.RadioButton();
+            this.lblFilesListed = new System.Windows.Forms.Label();
+            this.lblActiveFiles = new System.Windows.Forms.Label();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnRemoveFile = new System.Windows.Forms.Button();
+            this.btnMoveDown = new System.Windows.Forms.Button();
+            this.lbxFileList = new System.Windows.Forms.CheckedListBox();
+            this.btnSaveClose = new System.Windows.Forms.Button();
+            this.btnCheckAll = new System.Windows.Forms.Button();
+            this.btnUncheckAll = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.gbxSaveLocation = new System.Windows.Forms.GroupBox();
+            this.lblSaveFileLocation = new System.Windows.Forms.Label();
+            this.gbxListCount = new System.Windows.Forms.GroupBox();
+            this.gbxMergeOptions.SuspendLayout();
+            this.gbxSaveLocation.SuspendLayout();
+            this.gbxListCount.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Location = new System.Drawing.Point(11, 13);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(135, 37);
+            this.btnAddFile.TabIndex = 0;
+            this.btnAddFile.Text = "&Add File";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.btnAddFile_Click);
+            // 
+            // btnMergeExport
+            // 
+            this.btnMergeExport.Location = new System.Drawing.Point(853, 499);
+            this.btnMergeExport.Name = "btnMergeExport";
+            this.btnMergeExport.Size = new System.Drawing.Size(124, 110);
+            this.btnMergeExport.TabIndex = 15;
+            this.btnMergeExport.Text = "Merge and &Export ";
+            this.btnMergeExport.UseVisualStyleBackColor = true;
+            this.btnMergeExport.Click += new System.EventHandler(this.btnMergeExport_Click);
+            // 
+            // btnAddFolder
+            // 
+            this.btnAddFolder.Location = new System.Drawing.Point(152, 13);
+            this.btnAddFolder.Name = "btnAddFolder";
+            this.btnAddFolder.Size = new System.Drawing.Size(131, 37);
+            this.btnAddFolder.TabIndex = 1;
+            this.btnAddFolder.Text = "Add &Folder";
+            this.btnAddFolder.UseVisualStyleBackColor = true;
+            this.btnAddFolder.Click += new System.EventHandler(this.btnAddFolder_Click);
+            // 
+            // gbxMergeOptions
+            // 
+            this.gbxMergeOptions.Controls.Add(this.txtCommentText);
+            this.gbxMergeOptions.Controls.Add(this.radioButton1);
+            this.gbxMergeOptions.Controls.Add(this.rbtCommentDelimiter);
+            this.gbxMergeOptions.Controls.Add(this.rbtSpaceDelimiter);
+            this.gbxMergeOptions.Location = new System.Drawing.Point(12, 544);
+            this.gbxMergeOptions.Name = "gbxMergeOptions";
+            this.gbxMergeOptions.Size = new System.Drawing.Size(835, 65);
+            this.gbxMergeOptions.TabIndex = 18;
+            this.gbxMergeOptions.TabStop = false;
+            this.gbxMergeOptions.Text = "File Merge Options:  ";
+            // 
+            // txtCommentText
+            // 
+            this.txtCommentText.Location = new System.Drawing.Point(412, 26);
+            this.txtCommentText.Name = "txtCommentText";
+            this.txtCommentText.Size = new System.Drawing.Size(218, 20);
+            this.txtCommentText.TabIndex = 13;
+            this.txtCommentText.TextChanged += new System.EventHandler(this.txtCommentText_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(697, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(118, 17);
+            this.radioButton1.TabIndex = 14;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Do not use delimiter";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // rbtCommentDelimiter
+            // 
+            this.rbtCommentDelimiter.AutoSize = true;
+            this.rbtCommentDelimiter.Location = new System.Drawing.Point(191, 27);
+            this.rbtCommentDelimiter.Name = "rbtCommentDelimiter";
+            this.rbtCommentDelimiter.Size = new System.Drawing.Size(220, 17);
+            this.rbtCommentDelimiter.TabIndex = 12;
+            this.rbtCommentDelimiter.Text = "Insert text as comment tag between files: ";
+            this.rbtCommentDelimiter.UseVisualStyleBackColor = true;
+            // 
+            // rbtSpaceDelimiter
+            // 
+            this.rbtSpaceDelimiter.AutoSize = true;
+            this.rbtSpaceDelimiter.Location = new System.Drawing.Point(20, 27);
+            this.rbtSpaceDelimiter.Name = "rbtSpaceDelimiter";
+            this.rbtSpaceDelimiter.Size = new System.Drawing.Size(148, 17);
+            this.rbtSpaceDelimiter.TabIndex = 11;
+            this.rbtSpaceDelimiter.Text = "Insert space between files";
+            this.rbtSpaceDelimiter.UseVisualStyleBackColor = true;
+            // 
+            // lblFilesListed
+            // 
+            this.lblFilesListed.AutoSize = true;
+            this.lblFilesListed.Location = new System.Drawing.Point(12, 25);
+            this.lblFilesListed.Name = "lblFilesListed";
+            this.lblFilesListed.Size = new System.Drawing.Size(77, 13);
+            this.lblFilesListed.TabIndex = 16;
+            this.lblFilesListed.Text = "Files Listed:     ";
+            // 
+            // lblActiveFiles
+            // 
+            this.lblActiveFiles.AutoSize = true;
+            this.lblActiveFiles.Location = new System.Drawing.Point(11, 52);
+            this.lblActiveFiles.Name = "lblActiveFiles";
+            this.lblActiveFiles.Size = new System.Drawing.Size(79, 13);
+            this.lblActiveFiles.TabIndex = 17;
+            this.lblActiveFiles.Text = "Active Files:     ";
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Location = new System.Drawing.Point(647, 499);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(96, 44);
+            this.btnClearList.TabIndex = 2;
+            this.btnClearList.Text = "&Clear List";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.Location = new System.Drawing.Point(11, 499);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(108, 44);
+            this.btnMoveUp.TabIndex = 8;
+            this.btnMoveUp.Text = "Move &Up";
+            this.btnMoveUp.UseVisualStyleBackColor = true;
+            this.btnMoveUp.Click += new System.EventHandler(this.btnMoveUp_Click);
+            // 
+            // btnRemoveFile
+            // 
+            this.btnRemoveFile.Location = new System.Drawing.Point(749, 499);
+            this.btnRemoveFile.Name = "btnRemoveFile";
+            this.btnRemoveFile.Size = new System.Drawing.Size(98, 44);
+            this.btnRemoveFile.TabIndex = 9;
+            this.btnRemoveFile.Text = "&Remove File";
+            this.btnRemoveFile.UseVisualStyleBackColor = true;
+            this.btnRemoveFile.Click += new System.EventHandler(this.btnRemoveFile_Click);
+            // 
+            // btnMoveDown
+            // 
+            this.btnMoveDown.Location = new System.Drawing.Point(125, 499);
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(108, 44);
+            this.btnMoveDown.TabIndex = 10;
+            this.btnMoveDown.Text = "Move &Down";
+            this.btnMoveDown.UseVisualStyleBackColor = true;
+            this.btnMoveDown.Click += new System.EventHandler(this.btnMoveDown_Click);
+            // 
+            // lbxFileList
+            // 
+            this.lbxFileList.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lbxFileList.FormattingEnabled = true;
+            this.lbxFileList.Location = new System.Drawing.Point(12, 99);
+            this.lbxFileList.Name = "lbxFileList";
+            this.lbxFileList.ScrollAlwaysVisible = true;
+            this.lbxFileList.Size = new System.Drawing.Size(965, 394);
+            this.lbxFileList.TabIndex = 7;
+            this.lbxFileList.SelectedIndexChanged += new System.EventHandler(this.lbxFileList_SelectedIndexChanged);
+            // 
+            // btnSaveClose
+            // 
+            this.btnSaveClose.BackColor = System.Drawing.Color.NavajoWhite;
+            this.btnSaveClose.Location = new System.Drawing.Point(414, 13);
+            this.btnSaveClose.Name = "btnSaveClose";
+            this.btnSaveClose.Size = new System.Drawing.Size(105, 65);
+            this.btnSaveClose.TabIndex = 3;
+            this.btnSaveClose.Text = "&Save && Close";
+            this.btnSaveClose.UseVisualStyleBackColor = false;
+            this.btnSaveClose.Click += new System.EventHandler(this.btnSaveClose_Click);
+            // 
+            // btnCheckAll
+            // 
+            this.btnCheckAll.Location = new System.Drawing.Point(11, 55);
+            this.btnCheckAll.Name = "btnCheckAll";
+            this.btnCheckAll.Size = new System.Drawing.Size(135, 37);
+            this.btnCheckAll.TabIndex = 4;
+            this.btnCheckAll.Text = "Check All";
+            this.btnCheckAll.UseVisualStyleBackColor = true;
+            this.btnCheckAll.Click += new System.EventHandler(this.btnCheckAll_Click);
+            // 
+            // btnUncheckAll
+            // 
+            this.btnUncheckAll.Location = new System.Drawing.Point(152, 55);
+            this.btnUncheckAll.Name = "btnUncheckAll";
+            this.btnUncheckAll.Size = new System.Drawing.Size(131, 37);
+            this.btnUncheckAll.TabIndex = 5;
+            this.btnUncheckAll.Text = "Uncheck All";
+            this.btnUncheckAll.UseVisualStyleBackColor = true;
+            this.btnUncheckAll.Click += new System.EventHandler(this.btnUncheckAll_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(239, 499);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(108, 44);
+            this.btnSort.TabIndex = 6;
+            this.btnSort.Text = "Sort &List";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // gbxSaveLocation
+            // 
+            this.gbxSaveLocation.Controls.Add(this.lblSaveFileLocation);
+            this.gbxSaveLocation.Controls.Add(this.btnSaveClose);
+            this.gbxSaveLocation.Location = new System.Drawing.Point(451, 8);
+            this.gbxSaveLocation.Name = "gbxSaveLocation";
+            this.gbxSaveLocation.Size = new System.Drawing.Size(526, 85);
+            this.gbxSaveLocation.TabIndex = 19;
+            this.gbxSaveLocation.TabStop = false;
+            this.gbxSaveLocation.Text = "Save file location:  ";
+            // 
+            // lblSaveFileLocation
+            // 
+            this.lblSaveFileLocation.AutoSize = true;
+            this.lblSaveFileLocation.Location = new System.Drawing.Point(6, 22);
+            this.lblSaveFileLocation.Name = "lblSaveFileLocation";
+            this.lblSaveFileLocation.Size = new System.Drawing.Size(16, 13);
+            this.lblSaveFileLocation.TabIndex = 20;
+            this.lblSaveFileLocation.Text = "...";
+            // 
+            // gbxListCount
+            // 
+            this.gbxListCount.Controls.Add(this.lblFilesListed);
+            this.gbxListCount.Controls.Add(this.lblActiveFiles);
+            this.gbxListCount.Location = new System.Drawing.Point(289, 8);
+            this.gbxListCount.Name = "gbxListCount";
+            this.gbxListCount.Size = new System.Drawing.Size(155, 85);
+            this.gbxListCount.TabIndex = 20;
+            this.gbxListCount.TabStop = false;
+            this.gbxListCount.Text = "List item Count:  ";
+            // 
+            // frmFileMerge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 621);
+            this.Controls.Add(this.gbxListCount);
+            this.Controls.Add(this.gbxSaveLocation);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnCheckAll);
+            this.Controls.Add(this.btnUncheckAll);
+            this.Controls.Add(this.btnClearList);
+            this.Controls.Add(this.lbxFileList);
+            this.Controls.Add(this.btnMoveDown);
+            this.Controls.Add(this.btnRemoveFile);
+            this.Controls.Add(this.btnMoveUp);
+            this.Controls.Add(this.gbxMergeOptions);
+            this.Controls.Add(this.btnAddFolder);
+            this.Controls.Add(this.btnMergeExport);
+            this.Controls.Add(this.btnAddFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmFileMerge";
+            this.Text = "TRG File Merge";
+            this.Load += new System.EventHandler(this.frmFileMerge_Load);
+            this.gbxMergeOptions.ResumeLayout(false);
+            this.gbxMergeOptions.PerformLayout();
+            this.gbxSaveLocation.ResumeLayout(false);
+            this.gbxSaveLocation.PerformLayout();
+            this.gbxListCount.ResumeLayout(false);
+            this.gbxListCount.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.Button btnMergeExport;
+        private System.Windows.Forms.Button btnAddFolder;
+        private System.Windows.Forms.GroupBox gbxMergeOptions;
+        private System.Windows.Forms.Label lblFilesListed;
+        private System.Windows.Forms.Label lblActiveFiles;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnRemoveFile;
+        private System.Windows.Forms.Button btnMoveDown;
+        private System.Windows.Forms.CheckedListBox lbxFileList;
+        private System.Windows.Forms.Button btnSaveClose;
+        private System.Windows.Forms.Button btnCheckAll;
+        private System.Windows.Forms.Button btnUncheckAll;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rbtCommentDelimiter;
+        private System.Windows.Forms.RadioButton rbtSpaceDelimiter;
+        private System.Windows.Forms.TextBox txtCommentText;
+        private System.Windows.Forms.GroupBox gbxSaveLocation;
+        private System.Windows.Forms.Label lblSaveFileLocation;
+        private System.Windows.Forms.GroupBox gbxListCount;
+    }
+}
+
---
+++ TRG_File_Merge/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using TRG_File_Merge.Properties;
+
+namespace TRG_File_Merge
+{
+    public partial class frmFileMerge : Form
+    {
+        string destinationFolderPath = "";
+        int itemCount = 0;
+        string filesFolderPath = "";
+        string fileFolderPath = "";
+        string text = "";
+        string delimiter = "";
+        string saveFilePath = "";
+
+        public frmFileMerge()
+        {
+            InitializeComponent();
+            //look for and apply ini file
+            UpdateLists();
+            lbxFileList.HorizontalScrollbar = true;
+            LoadSaveFile();
+        }
+
+        private void btnAddFile_Click(object sender, EventArgs e)
+        {
+            //user browses to folder and selects file
+            OpenFileDialog fileSelected = new OpenFileDialog();
+            fileSelected.Filter = "Text Files (*.txt)|*.txt|XML Files (*.xml)|*.xml|SGML Files (*.sgml)|*.sgml|All Files (*.*)|*.*";
+            fileSelected.Multiselect = false;
+
+            if (fileSelected.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = fileSelected.FileName;
+                //file gets added to listbox
+                lbxFileList.Items.Add(selectedFileName, true);
+            }
+            UpdateLists();
+        }
+
+        private void WriteToSaveFile ()
+        {
+            //create a save file with the current listbox items in it
+        }
+
+        private void LoadSaveFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Brian Macdonald\Desktop\save.sav");
+            foreach (string listItem in lines)
+            {
+                lbxFileList.Items.Add(listItem, true);
+            }
+
+        }
+
+        private void btnAddFolder_Click(object sender, EventArgs e)
+        {
+            //user browses to folder
+            FolderBrowserDialog filesPath = new System.Windows.Forms.FolderBrowserDialog();
+            if (DialogResult.OK == filesPath.ShowDialog())
+            {
+                filesFolderPath = Convert.ToString(filesPath.SelectedPath);
+                string[] filesSelected = Directory.GetFiles(filesFolderPath);
+
+                foreach (string fileName in filesSelected)
+                {
+                    //all files from folder are added to listbox
+                    lbxFileList.Items.Add(fileName, true);
+                }
+            }
+
+            //file path from each file is added to mergelist
+            UpdateLists();
+        }
+
+        private void btnMergeExport_Click(object sender, EventArgs e)
+        {
+            if (rbtSpaceDelimiter.Checked == true)
+            {
+                delimiter = "\n\n\n\n\n";
+            }
+            else if (rbtCommentDelimiter.Checked == true)
+            {
+                delimiter = "\n\n<!-- " + txtCommentText.Text + " -->\n\n";
+            }
+            else
+            {
+                delimiter = "";
+            }
+
+            foreach (string Item in lbxFileList.CheckedItems)
+            {
+                //don't use delimiter if it's the last iteration of the foreach loop
+                if (lbxFileList.CheckedItems.IndexOf(Item) == lbxFileList.CheckedItems.Count - 1)
+                {
+                    delimiter = "";
+                }
+                
+                //all files from folder are added to listbox
+                text += System.IO.File.ReadAllText(Item);
+                text += delimiter;
+            }
+
+            var sfd = new SaveFileDialog();
+
+            sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            sfd.FilterIndex = 2;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, text);
+                MessageBox.Show("Your file has been exported.\n\nFile Location:\t" + sfd.FileName);
+                text = "";
+            }
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            if (lbxFileList.Items.Count == 0)
+            {
+                MessageBox.Show("List is already empty.");
+            }
+            else
+            {
+                //message box asks user to confirm the clearing of the list
+                DialogResult dialogResult = MessageBox.Show("Are you sure you want to clear the entire list?", "Clear List?", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    lbxFileList.Items.Clear();
+                }
+            }
+            //after user confirmation, filenames are cleared from listbox
+            UpdateLists();
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e)
+        {
+            //check if anything is selected
+            if (lbxFileList.SelectedIndex == -1)
+            {
+                MessageBox.Show("Select an Item first!");
+            }
+            else
+            {
+                MoveItem(-1);
+            }
+        }
+
+        private void btnMoveDown_Click(object sender, EventArgs e)
+        {
+            //check if anything is selected
+            if (lbxFileList.SelectedIndex == -1)
+            {
+                MessageBox.Show("Select an Item first!");
+            }
+            else
+            {
+                MoveItem(1);
+            }
+        }
+
+        private void MoveItem(int direction)
+        {
+            // Checking selected item
+            if (lbxFileList.SelectedItem == null || lbxFileList.SelectedIndex < 0)
+                return; // No selected item - nothing to do
+
+            // Calculate new index using move direction
+            int newIndex = lbxFileList.SelectedIndex + direction;
+
+            // Checking bounds of the range
+            if (newIndex < 0 || newIndex >= lbxFileList.Items.Count)
+                return; // Index out of range - nothing to do
+
+            object selected = lbxFileList.SelectedItem;
+
+            // Removing selected item
+            lbxFileList.Items.Remove(selected);
+            // Insert selected item in new position
+            lbxFileList.Items.Insert(newIndex, selected);
+            // keep selected item selected (re-select)
+            lbxFileList.SetSelected(newIndex, true);
+
+            UpdateLists();
+        }
+
+        private void btnRemoveFile_Click(object sender, EventArgs e)
+        {
+            //message box asks user to confirm the remove
+            if (lbxFileList.SelectedIndex == -1)
+            {
+                MessageBox.Show("Select an Item first!");
+            }
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("Remove this item from the list?", "Remove Item?", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    //remove file from listbox
+                    lbxFileList.Items.Remove(lbxFileList.SelectedItem);
+                    UpdateLists();
+                }
+            }
+        }
+
+        private void UpdateLists()
+        {
+            lblFilesListed.Text = "Files Listed:     " + Convert.ToString(lbxFileList.Items.Count);
+            lblActiveFiles.Text = "Active Files:     " + Convert.ToString(lbxFileList.CheckedItems.Count);
+        }
+
+        private void btnSaveClose_Click(object sender, EventArgs e)
+        {
+            //Save settings
+            Settings.Default["CommentText"] = txtCommentText.Text;
+            Settings.Default.Save();
+            WriteToSaveFile();
+
+            this.Close();
+        }
+
+        private void btnCheckAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lbxFileList.Items.Count; i++)
+            {
+                lbxFileList.SetItemChecked(i, true);
+            }
+            UpdateLists();
+        }
+
+        private void btnUncheckAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lbxFileList.Items.Count; i++)
+            {
+                lbxFileList.SetItemChecked(i, false);
+            }
+            UpdateLists();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            lbxFileList.Sorted = true;
+        }
+
+        private void txtCommentText_TextChanged(object sender, EventArgs e)
+        {
+            rbtCommentDelimiter.Checked = true;
+        }
+
+        private void lbxFileList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateLists();
+        }
+
+        private void frmFileMerge_Load(object sender, EventArgs e)
+        {
+            txtCommentText.Text = Settings.Default["CommentText"].ToString();
+        }
+    }
+}
